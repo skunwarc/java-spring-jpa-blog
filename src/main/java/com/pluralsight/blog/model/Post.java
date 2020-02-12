d50f6d12feb2bf127670716e8b92fa880625da08
Sagar Kunwar Chhetri <sagar_kunwar@outlook.com>
2020-02-11 22:06:18 -0500

    sagar kunwar commit

--- src/main/java/com/pluralsight/blog/model/Post.java
+++ src/main/java/com/pluralsight/blog/model/Post.java
@@ -5,79 +5,84 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
+@Entity
 public class Post {
-    private Long id;
-    private String title;
-    private String lead;
-    private String body;
-    private String author;
-    private Date date;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(length = 1000000)
+	@Lob
+	private Long id;
+	private String title;
+	private String lead;
+	private String body;
+	private String author;
+	@Temporal(TemporalType.DATE)
+	private Date date;
 
-    public Post() {
-        super();
-    }
+	public Post() {
+		super();
+	}
 
-    public Post(Long id, String title, String lead, String body, String author, Date date) {
-        this();
-        this.id = id;
-        this.title = title;
-        this.lead = lead;
-        this.body = body;
-        this.author = author;
-        this.date = date;
-    }
+	public Post(Long id, String title, String lead, String body, String author, Date date) {
+		this();
+		this.id = id;
+		this.title = title;
+		this.lead = lead;
+		this.body = body;
+		this.author = author;
+		this.date = date;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public String getLead() {
-        return lead;
-    }
+	public String getLead() {
+		return lead;
+	}
 
-    public void setLead(String lead) {
-        this.lead = lead;
-    }
+	public void setLead(String lead) {
+		this.lead = lead;
+	}
 
-    public String getBody() {
-        return body;
-    }
+	public String getBody() {
+		return body;
+	}
 
-    public void setBody(String body) {
-        this.body = body;
-    }
+	public void setBody(String body) {
+		this.body = body;
+	}
 
-    public String getAuthor() {
-        return author;
-    }
+	public String getAuthor() {
+		return author;
+	}
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
+	public void setAuthor(String author) {
+		this.author = author;
+	}
 
-    public Date getDate() {
-        return date;
-    }
+	public Date getDate() {
+		return date;
+	}
 
-    public String getDateStr() {
-        DateFormat outputFormatter = new SimpleDateFormat("MM/dd/yyyy");
-        return outputFormatter.format(this.date);
-    }
+	public String getDateStr() {
+		DateFormat outputFormatter = new SimpleDateFormat("MM/dd/yyyy");
+		return outputFormatter.format(this.date);
+	}
 
-    public void setDate(Date date) {
-        this.date = date;
-    }
+	public void setDate(Date date) {
+		this.date = date;
+	}
 }
